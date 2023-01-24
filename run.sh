6f8893b43b28e18bd5980cfdb6a4a423f46aa9c9
Rachel House <rachel.house@everactive.com>
2023-01-24 11:29:12 -0500

    scripts to dockerize and run notebook

---
+++ run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Everactive ENV+ EvalKit Exploration Notebook (Dockerized)"
+echo ""
+
+# Start a Docker container with the current repo directory mounted as a volume.
+echo "Starting Docker container..."
+CONTAINER_ID=`docker run -it -d --rm \
+    --name everactive-envplus-sample-notebook \
+    -p 8888:8888 \
+    -v $PWD:/repo \
+everactive-envplus bash` > /dev/null
+
+# Install notebook dependencies via the poetry and create an ipykernel for the
+# Jupyterlab notebook.
+echo "Installing dependencies with poetry..."
+docker exec $CONTAINER_ID bash -c 'cd /repo; poetry install' > /dev/null 2>&1
+
+echo "Creating ipykernel for notebook use..."
+docker exec $CONTAINER_ID bash -c 'cd /repo; poetry run python -m ipykernel install --user --name=everactive-envplus' > /dev/null 2>&1
+
+# Start Jupyterlab.
+echo "Starting JupyterLab..."
+docker exec $CONTAINER_ID bash -c 'cd /repo; jupyter lab --ip=0.0.0.0 --no-browser --allow-root &' > /dev/null 2>&1
+
+# Pull out running notebook token and display localhost URL for user.
+RUNNING_LAB="$(docker exec $CONTAINER_ID bash -c 'jupyter lab list 2>&1')"
+RUNNING_LAB=$(echo $RUNNING_LAB | tr '\n' ' ')
+
+TOKEN_REGEX=".*token=([[:alnum:]]+)[[:space:]]+"
+echo ""
+
+if [[ $RUNNING_LAB =~ $TOKEN_REGEX ]]
+then
+    echo "Copy and and paste this URL into your web browser to access JupyterLab:"
+    echo "http://127.0.0.1:8888/lab?token=${BASH_REMATCH[1]}"
+else
+    echo "Could not get JupyterLab localhost URL."
+fi
+
+echo ""
+read -p "Press ENTER to stop JupyterLab and stop the Docker container."
+
+# Stop the Docker container.
+echo ""
+echo "Stopping JupyterLab and Docker container..."
+docker container stop $CONTAINER_ID  > /dev/null
+
+echo "Done."
